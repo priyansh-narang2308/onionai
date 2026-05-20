78ed84579da10d8a5b4abd0aa055b7d7984ca45d
supermanhenry <145008344+priyansh-narang2308@users.noreply.github.com>
2026-05-20 22:16:59 +0530

    feat: integrate Clerk authentication, add project dependencies, and configure environment and UI scaffolding

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+    ],    
+  },
+  allowedDevOrigins:[
+    "yolande-sistroid-jenee.ngrok-free.dev"
+  ]
 };
 
 export default nextConfig;
